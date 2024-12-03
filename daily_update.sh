8244dead087614afb97ba6711be9c04dbde1124a
Tnayin Server <khachatryandavid1@gmail.com>
2024-12-03 04:35:51 +0400

    Update files

--- daily_update.sh
+++ daily_update.sh
@@ -3,6 +3,7 @@ cd ~/nu-msr-search-engine/
 source /home/david/nu-msr-search-engine/venv/bin/activate
 cd
 python3 ~/nu-msr-search-engine/get_files.py /home/david/go/bin/katana
+cp downloaded_html/ nu-msr-search-engine/downloaded_html/
 cd ~/nu-msr-search-engine/
 git add .
 git commit -m "Update files"
